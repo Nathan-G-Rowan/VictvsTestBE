e818e70088efb7ea49ddd1a4a3bf135a97ca25f3
Nathan-G-Rowan <nathangrowan@gmail.com>
2023-04-23 16:03:13 +0100

    begin implementing exams table

--- db/seed.js
+++ db/seed.js
@@ -2,23 +2,37 @@ const db = require("./connection");
 const format = require("pg-format");
 
 const seed = async ({ candidateData }) => {
+  await db.query(`DROP TABLE IF EXISTS exams;`);
   await db.query(`DROP TABLE IF EXISTS candidates;`);
 
-  const candidateQuery = `
+  await db.query(`
     CREATE TABLE candidates (
       id SERIAL PRIMARY KEY,
-      name VARCHAR
-    );`;
-  await db.query(candidateQuery);
+      Name VARCHAR NOT NULL
+    );`);
+
+  await db.query(`
+    CREATE TABLE exams (
+      id SERIAL PRIMARY KEY,
+      Title VARCHAR NOT NULL,
+      Candidateid INTEGER NOT NULL REFERENCES candidates(id),
+      Date TIMESTAMP NOT NULL,
+      LocationName VARCHAR NOT NULL,
+      Latitude REAL NOT NULL,
+      Longitude REAL NOT NULL
+    );`);
 
   const insertCandidatesQuery = format(
-    "INSERT INTO candidates (name) VALUES %L RETURNING *;",
+    "INSERT INTO candidates (Name) VALUES %L RETURNING *;",
     candidateData.map(({ Name }) => [Name])
   );
+  await db.query(insertCandidatesQuery);
 
-  return db.query(insertCandidatesQuery).then((result) => {
-    return result.rows;
-});
+  const insertExamsQuery = format(
+    "INSERT INTO candidates (Name) VALUES %L RETURNING *;",
+    candidateData.map(({ Name }) => [Name])
+  );
+  await db.query(insertExamsQuery);
 };
 
 module.exports = seed;
