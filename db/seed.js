dd5c281a75d371676c9995a59cfac7fec4ee7efc
Nathan-G-Rowan <nathangrowan@gmail.com>
2023-04-24 11:44:58 +0100

    complete implementation of exam table and seeding data

--- db/seed.js
+++ db/seed.js
@@ -1,36 +1,56 @@
 const db = require("./connection");
 const format = require("pg-format");
 
-const seed = async ({ candidateData }) => {
+const seed = async ({ candidateData, examData }) => {
   await db.query(`DROP TABLE IF EXISTS exams;`);
   await db.query(`DROP TABLE IF EXISTS candidates;`);
+
 
   await db.query(`
     CREATE TABLE candidates (
       id SERIAL PRIMARY KEY,
-      Name VARCHAR NOT NULL
+      name VARCHAR NOT NULL
     );`);
 
   await db.query(`
     CREATE TABLE exams (
       id SERIAL PRIMARY KEY,
-      Title VARCHAR NOT NULL,
-      Candidateid INTEGER NOT NULL REFERENCES candidates(id),
-      Date TIMESTAMP NOT NULL,
-      LocationName VARCHAR NOT NULL,
-      Latitude REAL NOT NULL,
-      Longitude REAL NOT NULL
+      title VARCHAR NOT NULL,
+      description VARCHAR,
+      candidate_id INTEGER NOT NULL REFERENCES candidates(id),
+      date TIMESTAMP NOT NULL,
+      location_name VARCHAR NOT NULL,
+      latitude REAL NOT NULL,
+      longitude REAL NOT NULL
     );`);
 
   const insertCandidatesQuery = format(
-    "INSERT INTO candidates (Name) VALUES %L RETURNING *;",
+    "INSERT INTO candidates (name) VALUES %L RETURNING *;",
     candidateData.map(({ Name }) => [Name])
   );
   await db.query(insertCandidatesQuery);
 
   const insertExamsQuery = format(
-    "INSERT INTO candidates (Name) VALUES %L RETURNING *;",
-    candidateData.map(({ Name }) => [Name])
+    "INSERT INTO exams (title, description, candidate_id, date, location_name, latitude, longitude) VALUES %L RETURNING *;",
+    examData.map(
+      ({
+        Title,
+        Description,
+        Candidateid,
+        Date,
+        LocationName,
+        Latitude,
+        Longitude,
+      }) => [
+        Title,
+        Description,
+        Candidateid,
+        Date,
+        LocationName,
+        Latitude,
+        Longitude,
+      ]
+    )
   );
   await db.query(insertExamsQuery);
 };
