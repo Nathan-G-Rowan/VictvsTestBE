37d6e0b0fe4cf366741d1a2bc776d26c1e3ce89f
Nathan-G-Rowan <nathangrowan@gmail.com>
2023-04-24 13:55:57 +0100

    implement 404 response for unrecognised directories

--- app/app.js
+++ app/app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const { getApi } = require("./app.controllers");
+const { handle404Paths } = require("./error.controllers")
 
 const app = express();
 
 app.get("/api", getApi);
+app.get("*", handle404Paths);
 
 module.exports = app;
